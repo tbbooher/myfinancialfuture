3311230942f5fc84c012505247454d3423514ad2
Tim Booher <ads@theboohers.org>
2009-01-31 14:32:50 -0500

    first commit

---
+++ year.py
@@ -0,0 +1,13 @@
+class Year:
+    
+    def __init__(self,contents=None):
+        self.contents = contents or []
+
+    def federal_income_tax
+
+    def state_income_tax
+
+    def real_estate_taxes
+
+    
+
